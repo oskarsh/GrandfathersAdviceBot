005dbfd787254b49865d982727113576d9d2bef0
daeh <daeh@daehs-MBP.fritz.box>
2019-06-08 13:57:41 +0200

    added final touches

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 Basic example for a bot that uses inline keyboards.
 """
 import logging
-import requests
+import json
 import random
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
@@ -22,12 +22,14 @@ logger = logging.getLogger(__name__)
 
 
 # Ask the API to get the all the Data
-r = requests.get('https://my-json-server.typicode.com/daehruoydeef/GrandfathersAdviceBot/life')
-logger.info(r.json())
-life = r.json()
+with open('db.json') as json_file:  
+    data = json.load(json_file)
+    life = data["life"]
+    health = data["health"]
+    carrer = data["carrer"]
+    family = data["family"]
 
-
-def start(update, context):
+def advice(update, context):
     keyboard = [[InlineKeyboardButton("Life Advice", callback_data='1'),
                  InlineKeyboardButton("Well Being and Health ", callback_data='2'),
                  InlineKeyboardButton("Work and Carrer", callback_data='3'),
@@ -36,7 +38,10 @@ def start(update, context):
 ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose Time:', reply_markup=reply_markup)
+    update.message.reply_text('Please choose Category:', reply_markup=reply_markup)
+
+def start(update, context):
+    update.message.reply_text("Type /advice to get started")
 
 def button(update, context):
     query = update.callback_query
@@ -46,20 +51,25 @@ def button(update, context):
         logger.info("sending Family and Friends Advice")
         advice=random.choice(life)
         logger.info(advice)
-        query.edit_message_text(text=advice)
+        query.edit_message_text(text="\n \n "+ advice + " \n \n \n Get another /advice")
     elif data == "2":
         logger.info("sending Well Being and Health Advice")
+        advice=random.choice(health)
+        logger.info(advice)
+        query.edit_message_text(text="\n \n "+ advice + " \n \n \n Get another /advice")
     elif data == "3":
         logger.info("sending Work and Carrer Advice")
+        advice=random.choice(carrer)
+        logger.info(advice)
+        query.edit_message_text(text="\n \n "+ advice + " \n \n \n Get another /advice")
     elif data == "4":
         logger.info("sending Family and Friends Advice")
-
-    
-    # query.edit_message_text(text="Selected option: {}".format(query.data))
-
+        advice=random.choice(family)
+        logger.info(advice)
+        query.edit_message_text(text="\n \n "+ advice + " \n \n \n Get another /advice")
 
 def help(update, context):
-    update.message.reply_text("Use /start to test this bot.")
+    update.message.reply_text("Use /advice to get some advice.")
 
 
 def error(update, context):
@@ -68,15 +78,13 @@ def error(update, context):
 
 
 def main():
-
-
-
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("", use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('advice', advice))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_error_handler(error)
